9459a01d1ced441ba93cef71d8171a2f7e5637e7
krevordelapaz <krevordelapaz@yahoo.com>
2021-08-08 12:11:42 +0800

    Sourcode

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace FileStat
+{
+    partial class FileStatForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.getStatsButton = new System.Windows.Forms.Button();
+            this.findButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.statRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.findTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(17, 16);
+            this.openFileButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(141, 50);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "Open File";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // getStatsButton
+            // 
+            this.getStatsButton.Location = new System.Drawing.Point(17, 74);
+            this.getStatsButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.getStatsButton.Name = "getStatsButton";
+            this.getStatsButton.Size = new System.Drawing.Size(141, 50);
+            this.getStatsButton.TabIndex = 1;
+            this.getStatsButton.Text = "Get Stats";
+            this.getStatsButton.UseVisualStyleBackColor = true;
+            this.getStatsButton.Click += new System.EventHandler(this.getStatsButton_Click);
+            // 
+            // findButton
+            // 
+            this.findButton.Location = new System.Drawing.Point(17, 203);
+            this.findButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.findButton.Name = "findButton";
+            this.findButton.Size = new System.Drawing.Size(141, 36);
+            this.findButton.TabIndex = 2;
+            this.findButton.Text = "Find";
+            this.findButton.UseVisualStyleBackColor = true;
+            this.findButton.Click += new System.EventHandler(this.findButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(17, 246);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(141, 65);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // statRichTextBox
+            // 
+            this.statRichTextBox.Location = new System.Drawing.Point(187, 16);
+            this.statRichTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.statRichTextBox.Name = "statRichTextBox";
+            this.statRichTextBox.Size = new System.Drawing.Size(571, 294);
+            this.statRichTextBox.TabIndex = 4;
+            this.statRichTextBox.Text = "";
+            // 
+            // findTextBox
+            // 
+            this.findTextBox.Location = new System.Drawing.Point(17, 171);
+            this.findTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.findTextBox.Name = "findTextBox";
+            this.findTextBox.Size = new System.Drawing.Size(140, 22);
+            this.findTextBox.TabIndex = 0;
+            // 
+            // FileStatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 325);
+            this.Controls.Add(this.findTextBox);
+            this.Controls.Add(this.statRichTextBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.findButton);
+            this.Controls.Add(this.getStatsButton);
+            this.Controls.Add(this.openFileButton);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FileStatForm";
+            this.Text = "File Stats";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Button getStatsButton;
+        private System.Windows.Forms.Button findButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.RichTextBox statRichTextBox;
+        private System.Windows.Forms.TextBox findTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace FileStat
+{
+    public partial class FileStatForm : Form
+    {
+        string[] words;
+        string fileContent;
+        string fileName;
+
+        public FileStatForm()
+        {
+            InitializeComponent();
+
+            getStatsButton.Enabled = false;
+            findButton.Enabled = false;
+            exitButton.Enabled = false;
+        }
+
+        private void openFileButton_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                dialog.Filter = "Text|*.txt|All|*.*";
+                DialogResult result = dialog.ShowDialog();
+                if(result == DialogResult.OK)
+                {
+                    fileName = dialog.FileName;
+                    OpenFileDialog(fileName);
+                }
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+        private void OpenFileDialog(string fileName)
+        {
+            fileContent = File.ReadAllText(fileName);
+            getStatsButton.Enabled = true;
+            findButton.Enabled = true;
+            exitButton.Enabled = true;
+        }
+
+        private void getStatsButton_Click(object sender, EventArgs e)
+        {
+            words = fileContent.Split(' ');
+
+            int count = words.Count();
+            string headerFile = String.Format("File name:{0}\nNumber of words:{1}\n\nFrequency of letters:\n", Path.GetFileName(fileName), count);
+
+            if (words != null && words.Count() > 0)
+            {
+
+                // Array to store frequencies.
+                int[] characters = new int[(int)char.MaxValue];
+
+                var freqs = fileContent
+                    .Where(c => Char.IsLetter(c))
+                    .GroupBy(c => char.ToUpper(c))
+                    .ToDictionary(g => g.Key, g => g.Count());
+
+                string letters = string.Empty;
+
+                foreach (var letter in freqs.OrderBy(i => i.Key))
+                {
+                    letters += char.ToLower(letter.Key) + ": " + letter.Value + "\t";
+                }
+
+                statRichTextBox.Text += headerFile + letters;
+            }
+            else
+            {
+                MessageBox.Show("Text file does not contain any words!");
+            }
+        }
+
+        private void findButton_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(findTextBox.Text))
+            {
+                var count = Regex.Matches(fileContent, findTextBox.Text).Count;
+
+                statRichTextBox.Text += string.Format("\nNo of times {0} appear on the file: {1}", findTextBox.Text, count);
+            }
+        }
+    }
+}
